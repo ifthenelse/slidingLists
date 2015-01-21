02e7d6483685131223cd9438c9c45481627453ca
Andrea Collet <collet.andrea@gmail.com>
2015-01-22 00:18:14 +0100

    Added Gruntfile configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 
 		// Import package manifest
-		pkg: grunt.file.readJSON("boilerplate.jquery.json"),
+		pkg: grunt.file.readJSON("slidinglists.jquery.json"),
 
 		// Banner definitions
 		meta: {
@@ -20,8 +20,8 @@ module.exports = function(grunt) {
 		// Concat definitions
 		concat: {
 			dist: {
-				src: ["src/jquery.boilerplate.js"],
-				dest: "dist/jquery.boilerplate.js"
+				src: ["src/jquery.slidinglists.js"],
+				dest: "dist/jquery.slidinglists.js"
 			},
 			options: {
 				banner: "<%= meta.banner %>"
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
 
 		// Lint definitions
 		jshint: {
-			files: ["src/jquery.boilerplate.js"],
+			files: ["src/jquery.slidinglists.js"],
 			options: {
 				jshintrc: ".jshintrc"
 			}
@@ -39,8 +39,8 @@ module.exports = function(grunt) {
 		// Minify definitions
 		uglify: {
 			my_target: {
-				src: ["dist/jquery.boilerplate.js"],
-				dest: "dist/jquery.boilerplate.min.js"
+				src: ["dist/jquery.slidinglists.js"],
+				dest: "dist/jquery.slidinglists.min.js"
 			},
 			options: {
 				banner: "<%= meta.banner %>"
@@ -51,13 +51,13 @@ module.exports = function(grunt) {
 		coffee: {
 			compile: {
 				files: {
-					"dist/jquery.boilerplate.js": "src/jquery.boilerplate.coffee"
+					"dist/jquery.slidinglists.js": "src/jquery.slidinglists.coffee"
 				}
 			}
 		},
-		
-		// watch for changes to source 
-		// Better than calling grunt a million times 
+
+		// watch for changes to source
+		// Better than calling grunt a million times
 		// (call 'grunt watch')
 		watch: {
 		    files: ['src/*'],
